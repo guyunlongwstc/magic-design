23e90ec1d030b912219b289e2a41620e73b82bb8
guyunlong <guyunlong@baidu.com>
2020-11-26 15:48:22 +0800

    react-router

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,21 @@
  * @author guyunlong
  */
 
-import App from './app';
 import {Provider} from 'react-redux';
 import * as reducers from '@reducers';
 import {createStore, combineReducers} from 'redux';
 import ReactDOM from 'react-dom';
+import {BrowserRouter } from 'react-router-dom';
 import React from 'react';
+import {Route} from 'react-router';
+import App from './app';
 
-const store = createStore(combineReducers(reducers))
+const store = createStore(combineReducers(reducers,));
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <Route path="/" component={App} />
+        </BrowserRouter>
     </Provider>,
 document.getElementById('main'));
